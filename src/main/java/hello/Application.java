f2bf50c5bb14b2cc2aba6196575cb76191d5a651
Santiago <santidesimone@ar.ibm.com>
2019-11-18 14:19:01 -0300

    added area objects to the cache

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -23,69 +23,89 @@ public class Application {
             .build());
       Cache<Object, Object> cache = manager.getCache("custom-cache");
 
-      System.out.println("");
-      System.out.println("Add a entry");
-      cache.put("key", "value");
-      System.out.println("added 'key' as the key, and 'value' as the value for this entry");
-      System.out.println("--------------------------------------");
-      System.out.println("");
-
-      System.out.println("Validate the entry is now in the cache");
-      assertEqual(1, cache.size());
-      System.out.println("cache.size() is: " + cache.size() );
-      assertTrue(cache.containsKey("key"));
-      System.out.println("--------------------------------------");
-      System.out.println("");
-
-
-
-      System.out.println("Remove the entry from the cache");
-      Object v = cache.remove("key");
-      System.out.println("--------------------------------------");
-      System.out.println("");
-
-
-      System.out.println("Validate the entry is no longer in the cache");
-      assertEqual("value", v);
-      assertTrue(cache.isEmpty());
-      System.out.println("cache.isEmpty() results in : " + cache.isEmpty() );
-      System.out.println("--------------------------------------");
-      System.out.println("");
-
-
-
-      System.out.println("Add an entry with the key 'key'");
-      cache.put("key", "value");
-      System.out.println("And replace its value only if the key missing");
-      cache.putIfAbsent("key", "newValue");
-      System.out.println("Validate that the new value was not added");
-      assertEqual("value", cache.get("key"));
-      System.out.println("cache.get('key') = " + cache.get("key") );
-      System.out.println("--------------------------------------");
-      System.out.println("");
-
-
-      cache.clear();
-      assertTrue(cache.isEmpty());
-
-      
-      System.out.println("cache.clear() was executed");
-      cache.clear();
-      assertTrue(cache.isEmpty());
-      System.out.println("cache.isEmpty() results in : " + cache.isEmpty() );
-      System.out.println("--------------------------------------");
-      System.out.println("");
-
-
-      //By default entries are immortal but we can override this on a per-key basis and provide lifespans.
-      cache.put("key", "value", 5, SECONDS);
-      assertTrue(cache.containsKey("key"));
-      Thread.sleep(10000);
-      assertFalse(cache.containsKey("key"));
-
 
       Area area1 = new Area("TIGRE", "TG", "Partido");
-      System.out.println(area1.toString());
+      // System.out.println(area1.toString());
+
+
+      System.out.println("");
+      System.out.println("Add a entry");
+      cache.put("Area1", area1);
+      System.out.println("added 'Area1' as the key, and area1 object as the value for this entry");
+      System.out.println("--------------------------------------");
+      System.out.println("");
+
+      
+      Area area2 = new Area("SAN ISIDRO", "SI", "Partido");
+      System.out.println("");
+      System.out.println("Add a entry");
+      cache.put("Area2", area2);
+      System.out.println("added 'Area2' as the key, and area2 object as the value for this entry");
+      System.out.println("--------------------------------------");
+      System.out.println("");
+
+      Area area3 = new Area("CORDOBA", "CB", "Provincia");
+      System.out.println("");
+      System.out.println("Add a entry");
+      cache.put("Area3", area3);
+      System.out.println("added 'Area3' as the key, and area3 object as the value for this entry");
+      System.out.println("--------------------------------------");
+      System.out.println("");
+
+
+      System.out.println("Validate the entry is now in the cache");
+      System.out.println("cache.size() is: " + cache.size() );
+      System.out.println("--------------------------------------");
+      System.out.println("");
+
+
+
+      // System.out.println("Remove the entry from the cache");
+      // Object v = cache.remove("key");
+      // System.out.println("--------------------------------------");
+      // System.out.println("");
+
+
+      // System.out.println("Validate the entry is no longer in the cache");
+      // assertEqual("value", v);
+      // assertTrue(cache.isEmpty());
+      // System.out.println("cache.isEmpty() results in : " + cache.isEmpty() );
+      // System.out.println("--------------------------------------");
+      // System.out.println("");
+
+
+
+      // System.out.println("Add an entry with the key 'key'");
+      // cache.put("key", "value");
+      // System.out.println("And replace its value only if the key missing");
+      // cache.putIfAbsent("key", "newValue");
+      // System.out.println("Validate that the new value was not added");
+      // assertEqual("value", cache.get("key"));
+      // System.out.println("cache.get('key') = " + cache.get("key") );
+      // System.out.println("--------------------------------------");
+      // System.out.println("");
+
+
+      // cache.clear();
+      // assertTrue(cache.isEmpty());
+
+      
+      // System.out.println("cache.clear() was executed");
+      // cache.clear();
+      // assertTrue(cache.isEmpty());
+      // System.out.println("cache.isEmpty() results in : " + cache.isEmpty() );
+      // System.out.println("--------------------------------------");
+      // System.out.println("");
+
+
+      // //By default entries are immortal but we can override this on a per-key basis and provide lifespans.
+      // cache.put("key", "value", 5, SECONDS);
+      // assertTrue(cache.containsKey("key"));
+      // Thread.sleep(10000);
+      // assertFalse(cache.containsKey("key"));
+
+
+
       
    }
 
